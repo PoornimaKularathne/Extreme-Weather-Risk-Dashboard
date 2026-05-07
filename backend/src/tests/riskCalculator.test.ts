c479a19a238545305e7aeca1d0b47be340ba610b
Poovy2 <poornimakularathne851@gmail.com>
2026-05-07 20:38:01 +0530

    code clarity

--- backend/src/tests/riskCalculator.test.ts
+++ backend/src/tests/riskCalculator.test.ts
@@ -2,14 +2,14 @@ import { calculateRisk } from "../utils/riskCalculator";
 
 test("stormy city should have higher risk than sunny city", () => {
 
-    // ☀️ Sunny city (low risk)
+    //  Sunny city (low risk)
     const sunnyCity = {
         main: { temp: 295 },
         wind: { speed: 2 },
         visibility: 10000
     };
 
-    // ⛈ Stormy city (high risk)
+    //  Stormy city (high risk)
     const stormyCity = {
         main: { temp: 310 },
         wind: { speed: 15 },
